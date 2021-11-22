3b5ce2cf0d6857e07b416ad18d2023cf25e41f01
jean PORCHER <jean.porcher@socgen.com>
2021-11-22 11:48:36 +0100

    ex03 done

--- ex03/App.js
+++ ex03/App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button
+        title="BUTTON"
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#808080",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  count: {
+    fontSize: 50,
   },
 });
